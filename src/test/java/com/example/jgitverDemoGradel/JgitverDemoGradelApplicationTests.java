11bba5672bab132c44bf485dbd77b1cc68365ff0
ponna <ponna_sunil@dell.com>
2021-03-13 11:54:37 +0530

    demo1

---
+++ src/test/java/com/example/jgitverDemoGradel/JgitverDemoGradelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jgitverDemoGradel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JgitverDemoGradelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
