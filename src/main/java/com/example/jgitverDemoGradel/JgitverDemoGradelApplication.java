5e4e82d51bb494daa778c66bcf684281adc91d9d
ponna <ponna_sunil@dell.com>
2021-03-13 16:01:05 +0530

    demo2

--- src/main/java/com/example/jgitverDemoGradel/JgitverDemoGradelApplication.java
+++ src/main/java/com/example/jgitverDemoGradel/JgitverDemoGradelApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class JgitverDemoGradelApplication {
 
 	public static void main(String[] args) {
+		System.out.println("demo1")
 		SpringApplication.run(JgitverDemoGradelApplication.class, args);
 	}
 
